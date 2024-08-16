0295a5f52d09a427e02d2713eafdc11a21f59025
Lelik Jan <21alex7777@gmail.com>
2024-08-16 19:05:29 +0300

    feat(Checkbox): implement customizable Checkbox component
    - Create Checkbox component with customizable props (checked, disabled, label)
    - Implement forwarded ref for better integration
    - Add SCSS modules for styling with support for different states
    - Create Storybook stories to showcase various Checkbox states and behaviors
    
    This commit provides a fully functional and well-documented Checkbox component
    ready for use in the project, with comprehensive Storybook documentation.

--- src/components/ui/checkbox/checkbox.stories.tsx
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -1,9 +1,13 @@
-
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Checkbox } from './checkbox'
+import React from 'react'
 
 const meta = {
+  args: {
+    checked: true,
+    label: null,
+  },
   component: Checkbox,
   tags: ['autodocs'],
   title: 'Components/Checkbox',
@@ -12,6 +16,55 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  args: {},
+export const Default: Story = {}
+
+export const UnChecked: Story = {
+  args: {
+    checked: false,
+  },
+}
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
+export const DisabledUnchecked: Story = {
+  args: {
+    checked: false,
+    disabled: true,
+  },
+}
+export const ControlCheckbox: Story = {
+  render: args => {
+    const [checked, setChecked] = React.useState(false)
+
+    return <Checkbox {...args} checked={checked} onChange={setChecked} />
+  },
+}
+export const CheckedWithLabel: Story = {
+  args: {
+    label: 'checked checkbox',
+  },
+}
+export const UnCheckedWithLabel: Story = {
+  args: {
+    checked: false,
+    label: 'unchecked checkbox',
+  },
+}
+export const DisabledWithLabel: Story = {
+  args: {
+    disabled: true,
+    label: 'disabled checkbox',
+  },
+}
+export const ControlCheckboxWithLabel: Story = {
+  args: {
+    label: 'controlled  checkbox',
+  },
+  render: args => {
+    const [checked, setChecked] = React.useState(false)
+
+    return <Checkbox {...args} checked={checked} onChange={setChecked} />
+  },
 }
--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,13 +1,74 @@
+import { ElementRef, forwardRef, useId } from 'react'
+
+import * as CheckboxRadix from '@radix-ui/react-checkbox'
 import clsx from 'clsx'
 
-import styles from './checkbox.module.scss'
+import styles from './Checkbox.module.scss'
+import { CheckmarkOutline } from '@/assets'
 
-export type CheckboxProps = {}
-
-export const Checkbox = ({}: CheckboxProps) => {
-  const classNames = {
-    root: styles.root,
-  } as const
-
-  return <div className={clsx(classNames.root)}></div>
+type CheckboxProps = {
+  checked?: boolean
+  className?: string
+  disabled?: boolean
+  id?: string
+  label?: string | null
+  onChange?: (checked: boolean) => void
 }
+
+/**
+ * Checkbox component
+ *
+ * @param {CheckboxProps} props - The props for the Checkbox component
+ * @param {boolean} [props.checked] - The checked state of the checkbox
+ * @param {string} [props.className] - Additional CSS classes to apply to the container
+ * @param {boolean} [props.disabled] - Whether the checkbox is disabled
+ * @param {string} [props.id] - The unique identifier for the checkbox
+ * @param {string} [props.label] - The label text for the checkbox
+ * @param {(checked: boolean) => void} [props.onChange] - The callback function to be called when the checkbox state changes
+ * @returns {JSX.Element} - The Checkbox component
+ */
+
+export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>(
+  ({ checked, disabled, id, label, onChange }: CheckboxProps, ref) => {
+    const innerId = useId()
+    const finalId = id ?? innerId
+    const classNames = {
+      container: styles.container,
+      indicator: styles.indicator,
+      label: clsx(styles.label, disabled && styles.disabled),
+      root: clsx(styles.root, disabled && styles.disabled),
+      wrapper: clsx(styles.wrapper, disabled && styles.disabled),
+      icon: clsx(styles.icon, disabled && styles.disabled),
+    }
+
+    return (
+      <div className={clsx(classNames.container)}>
+        <div className={clsx(classNames.wrapper)}>
+          <CheckboxRadix.Root
+            checked={checked}
+            className={clsx(classNames.root)}
+            disabled={disabled}
+            id={finalId}
+            onCheckedChange={onChange}
+            ref={ref}
+          >
+            <CheckboxRadix.Indicator className={clsx(classNames.indicator)} forceMount>
+              {checked && <CheckmarkOutline className={classNames.icon} />}
+            </CheckboxRadix.Indicator>
+          </CheckboxRadix.Root>
+        </div>
+        {label && (
+          <label htmlFor={finalId} className={classNames.label}>
+            {label}
+          </label>
+        )}
+      </div>
+    )
+  }
+)
+
+/**
+ *This will help when debugging in React DevTools.
+ **/
+
+Checkbox.displayName = 'Checkbox'
