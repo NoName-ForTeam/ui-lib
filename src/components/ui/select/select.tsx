c9e9fbc05cca39bf4a8d692b67c2f8fe5d1aefe8
Emilo067 <gasanovemil467@gmail.com>
2024-08-17 13:12:49 +0300

    refactor: apply prettier formatting and fix stylelint warnings

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,60 +1,55 @@
-import * as SelectRadix from '@radix-ui/react-select';
+import * as SelectRadix from '@radix-ui/react-select'
 import styles from './select.module.scss'
 import Icon from '../../../assets/icons/components/ArrowIosDownOutline.tsx'
-import clsx from "clsx";
-import {ComponentPropsWithoutRef, ElementRef, forwardRef, ReactNode} from "react";
+import clsx from 'clsx'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, ReactNode } from 'react'
 
 export type Option = {
-    value: string
-    content: ReactNode | string
+  value: string
+  content: ReactNode | string
 }
 
 type SelectProps = {
-    label?: string
-    className?: string
-    options: Option[]
-    placeholder?: string
+  label?: string
+  className?: string
+  options: Option[]
+  placeholder?: string
 } & ComponentPropsWithoutRef<typeof SelectRadix.Root>
 
 export const Select = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectProps>((props, ref) => {
+  const { onValueChange, options, value, className, label, disabled, placeholder, ...rest } = props
 
-    const {
-        onValueChange,
-        options,
-        value,
-        className,
-        label,
-        disabled,
-        placeholder,
-        ...rest
-    } = props;
-
-    return (
-            <SelectRadix.Root onValueChange={onValueChange} value={value} {...rest}>
-                <div className={styles.container}>
-                    <label className={clsx(styles.label, className)}>
-                        {label}
-                        <SelectRadix.Trigger ref={ref} className={clsx(styles.trigger, !label?.length && styles.withoutLabel, disabled && styles.disabled)}>
-                            <SelectRadix.Value placeholder={placeholder ?? '...'}/>
-                            <SelectRadix.Icon asChild>
-                                <Icon className={styles.icon}/>
-                            </SelectRadix.Icon>
-                        </SelectRadix.Trigger>
-                    </label>
-                </div>
-                <SelectRadix.Portal>
-                    <SelectRadix.Content position={"popper"} className={styles.content}>
-                        <SelectRadix.Viewport>
-                            {options.map((option) => (
-                                <SelectRadix.Item className={styles.item} key={option.value} value={option.value}>
-                                    <SelectRadix.ItemText>
-                                        {option.content}
-                                    </SelectRadix.ItemText>
-                                </SelectRadix.Item>
-                            ))}
-                        </SelectRadix.Viewport>
-                    </SelectRadix.Content>
-                </SelectRadix.Portal>
-            </SelectRadix.Root>
-    )
+  return (
+    <SelectRadix.Root onValueChange={onValueChange} value={value} {...rest}>
+      <div className={styles.container}>
+        <label className={clsx(styles.label, className)}>
+          {label}
+          <SelectRadix.Trigger
+            ref={ref}
+            className={clsx(
+              styles.trigger,
+              !label?.length && styles.withoutLabel,
+              disabled && styles.disabled
+            )}
+          >
+            <SelectRadix.Value placeholder={placeholder ?? '...'} />
+            <SelectRadix.Icon asChild>
+              <Icon className={styles.icon} />
+            </SelectRadix.Icon>
+          </SelectRadix.Trigger>
+        </label>
+      </div>
+      <SelectRadix.Portal>
+        <SelectRadix.Content position={'popper'} className={styles.content}>
+          <SelectRadix.Viewport>
+            {options.map(option => (
+              <SelectRadix.Item className={styles.item} key={option.value} value={option.value}>
+                <SelectRadix.ItemText>{option.content}</SelectRadix.ItemText>
+              </SelectRadix.Item>
+            ))}
+          </SelectRadix.Viewport>
+        </SelectRadix.Content>
+      </SelectRadix.Portal>
+    </SelectRadix.Root>
+  )
 })
