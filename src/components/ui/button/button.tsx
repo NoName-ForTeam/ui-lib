2357f3d24c19dcc9d18970c29bd9638ef5e15e14
Lelik Jan <21alex7777@gmail.com>
2024-08-19 09:07:48 +0300

    feature(button): add button folder with templates
    - Remove old folder button with files

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,23 +1,15 @@
-import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
-import s from './button.module.scss'
-import clsx from 'clsx'
-import { Slot } from '@radix-ui/react-slot'
 
-type Props = {
-  asChild?: boolean
-  variant?: 'primary' | 'secondary' | 'outlined' | 'ghost'
-  fullWidth?: boolean
-} & ComponentPropsWithoutRef<'button'>
+  import clsx from 'clsx'
 
-export const Button = forwardRef<ElementRef<'button'>, Props>(
-  ({ variant = 'primary', fullWidth, className, asChild, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button'
-    return (
-      <Comp
-        {...props}
-        ref={ref}
-        className={clsx(s.buttonRoot, s[variant], fullWidth && s.fullWidth, className)}
-      />
-    )
+  import styles from './button.module.scss'
+
+  export type ButtonProps = {}
+
+  export const Button = ({}:ButtonProps) => {
+   const classNames = {
+    root: styles.root,
+  } as const
+
+  return <div className={clsx(classNames.root)}></div>
   }
-)
+  
