520a46a5f6691b4c52c8df0b161732f78f97bcf1
Emilo067 <gasanovemil467@gmail.com>
2024-08-20 18:39:14 +0300

    refactor: apply requested changes

--- src/components/ui/cards/cards.stories.tsx
+++ src/components/ui/cards/cards.stories.tsx
@@ -1,8 +1,8 @@
 import { type Meta, type StoryObj } from '@storybook/react'
 
 import { Cards } from './cards'
-import {Typography} from "../typography";
-import {Button} from "../button";
+import { Typography } from '../typography'
+import { Button } from '../button'
 
 const meta = {
   component: Cards,
@@ -23,15 +23,15 @@ export const Default = {
 
 export const CardsContent = () => {
   return (
-      <Cards style={{ width: '420px', padding: '26px' }}>
-        <Typography as={'h1'} variant={'h1'}>
-          Learn &quot;Deck Name&quot;
-        </Typography>
-        <Typography variant={'textBold16'}>
-          Question: How &quot;This&quot; works in JavaScript?
-        </Typography>
-        <Typography>Количество попыток ответов на вопрос: 10</Typography>
-        <Button fullWidth>Show Answer</Button>
-      </Cards>
+    <Cards style={{ width: '420px', padding: '26px' }}>
+      <Typography as={'h1'} variant={'h1'}>
+        Learn &quot;Deck Name&quot;
+      </Typography>
+      <Typography variant={'textBold16'}>
+        Question: How &quot;This&quot; works in JavaScript?
+      </Typography>
+      <Typography>Количество попыток ответов на вопрос: 10</Typography>
+      <Button fullWidth>Show Answer</Button>
+    </Cards>
   )
 }
