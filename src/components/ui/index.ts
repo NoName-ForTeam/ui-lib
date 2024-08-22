660737e9b5286fdc51b5f7a29e579653c8f02011
Lelik Jan <21alex7777@gmail.com>
2024-08-22 12:28:54 +0300

    Resolve merge conflict in src/components/ui/index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 export * from './recaptcha'
-export * from './textArea'
-=======
 export * from './textArea'
 export * from './typography'
 export * from './cards'
->>>>>>> dev
 export * from './checkbox'
 export * from './select'
 export * from './button'
