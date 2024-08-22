0c2a0f9793dba9e8fef9efcb3e533a9aa754093c
Lelik Jan <21alex7777@gmail.com>
2024-08-22 21:01:37 +0300

    feat(sidebars container - Implement SidebarsElement for individual sidebars items - Add styles for sidebars layout and interactions - Set up Storybook configuration with path aliases - Create initial Storybook story for Sidebars component - Add JSDoc comments for improved documentation - Set display names for debugging purposes
    BREAKING CHANGE: New Sidebars component structure may require updates to existing sidebar implementations

--- src/components/ui/sidebars/sidebars.stories.tsx
+++ src/components/ui/sidebars/sidebars.stories.tsx
@@ -1,7 +1,10 @@
-
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Sidebars } from './sidebars'
+import { Sidebars, SidebarsElement } from './sidebars'
+
+import { Home } from '@/assets'
+import { Button } from '../button'
+import React from 'react'
 
 const meta = {
   component: Sidebars,
@@ -13,5 +16,44 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    children: (
+      <>
+        <div
+          style={{ marginBottom: '60px', display: 'flex', flexDirection: 'column', gap: '24px' }}
+        >
+          <SidebarsElement>
+            <Home /> Home
+          </SidebarsElement>
+          <SidebarsElement>
+            <Home /> Create
+          </SidebarsElement>
+          <SidebarsElement>
+            <Home /> My profile
+          </SidebarsElement>
+          <SidebarsElement>
+            <Home /> Messenger
+          </SidebarsElement>
+          <SidebarsElement>
+            <Home /> Search
+          </SidebarsElement>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
+          <SidebarsElement>
+            <Home /> Statistics
+          </SidebarsElement>
+          <SidebarsElement disabled>
+            <Home />
+            Favorites
+          </SidebarsElement>
+        </div>
+        <div>
+          <SidebarsElement>
+            <Home />
+            Log Out
+          </SidebarsElement>
+        </div>
+      </>
+    ),
+  },
 }
--- src/components/ui/sidebars/sidebars.tsx
+++ src/components/ui/sidebars/sidebars.tsx
@@ -1,15 +1,58 @@
+import clsx from 'clsx'
 
-  import clsx from 'clsx'
+import styles from './sidebars.module.scss'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
-  import styles from './sidebars.module.scss'
+export type SidebarsProps = ComponentPropsWithoutRef<'div'>
 
-  export type SidebarsProps = {}
+/**
+ * Sidebars component.
+ * Renders a customizable sidebar container.
+ *
+ * @param {SidebarsProps} props - The component props.
+ * @param {React.Ref<HTMLDivElement>} ref - The forwarded ref.
+ * @returns {JSX.Element} The rendered Sidebars component.
+ */
 
-  export const Sidebars = ({}:SidebarsProps) => {
-   const classNames = {
-    root: styles.root,
-  } as const
+export const Sidebars = forwardRef<ElementRef<'div'>, SidebarsProps>(
+  ({ children, className, ...rest }: SidebarsProps, ref) => {
+    const classNames = {
+      root: clsx(styles.root, className),
+    } as const
 
-  return <div className={clsx(classNames.root)}></div>
+    return (
+      <div className={classNames.root} {...rest} ref={ref}>
+        {children}
+      </div>
+    )
   }
-  
+)
+
+type SidebarsElementProps = { disabled?: boolean } & ComponentPropsWithoutRef<'div'>
+
+/**
+ * SidebarsElement component.
+ * Renders a customizable element within the sidebar.
+ *
+ * @param {SidebarsElementProps} props - The component props.
+ * @param {React.Ref<HTMLDivElement>} ref - The forwarded ref.
+ * @returns {JSX.Element} The rendered SidebarsElement component.
+ */
+
+export const SidebarsElement = forwardRef<ElementRef<'div'>, SidebarsElementProps>(
+  ({ children, className, disabled, ...rest }, ref) => {
+    const classNames = {
+      element: clsx(styles.element, disabled && styles.disabled, className),
+    } as const
+    return (
+      <div className={classNames.element} {...rest} ref={ref}>
+        {children}
+      </div>
+    )
+  }
+)
+
+// Set display names for debugging
+
+Sidebars.displayName = 'Sidebars'
+SidebarsElement.displayName = 'SidebarsElement'
