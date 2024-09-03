afeb88ea0738c661e551c7b9318c4d58aa3b675e
Lelik Jan <21alex7777@gmail.com>
2024-09-03 22:59:33 +0300

    refactor(recaptcha): update recaptcha component and update stories

--- src/components/ui/recaptcha/recaptcha.tsx
+++ src/components/ui/recaptcha/recaptcha.tsx
@@ -1,9 +1,7 @@
 import clsx from 'clsx'
-import { ComponentPropsWithoutRef, useRef } from 'react'
+import { ComponentPropsWithoutRef, useCallback } from 'react'
 import ReCAPTCHAComponent from 'react-google-recaptcha'
 import styles from './recaptcha.module.scss'
-
-// type ReCaptchaStatus = 'default' | 'checked' | 'error'
 
 type ReCaptchaProps = {
   /**
@@ -14,43 +12,42 @@ type ReCaptchaProps = {
    * Indicates if there's an error state.
    */
   isError?: boolean
+  /**
+   * Callback function to handle reCAPTCHA response
+   */
+  onVerify: (token: string | null) => void
 } & ComponentPropsWithoutRef<'div'>
 
 /**
  * ReCaptcha component for user verification.
+ * @component
  * @example
- * <ReCaptcha siteKey="your-site-key" error={false} />
+ *  const [isError, setIsError] = useState(false);
+ * const handleVerify = (token: string | null) => {
+ *  if (token) {
+ *   setIsError(false);
+ *} else {
+ *   setIsError(true);
+ *}
+ *};
+ *
+ * <ReCaptcha siteKey="your-site-key" isError={false} onVerify={(token) => handleVerification(token)} />
  */
 
-export const ReCaptcha = ({ siteKey, isError, ...rest }: ReCaptchaProps) => {
-  // const [status, setStatus] = useState<ReCaptchaStatus>('default')
-  const reCaptchaRef = useRef<ReCAPTCHAComponent>(null)
-
-  /**
-   *may be needed to stylize the .container for different status in the future
-   */
-
-  // const handleChange = (value: string | null) => {
-  //   if (value) {
-  //     setStatus('checked')
-  //   } else {
-  //     setStatus('error')
-  //   }
-  // }
-
+export const ReCaptcha = ({ siteKey, isError, onVerify, ...rest }: ReCaptchaProps) => {
   const classNames = {
     container: clsx(styles.container, isError && styles.error),
     errorText: styles.errorText,
   } as const
+  const handleChange = useCallback(
+    (token: string | null) => {
+      onVerify(token)
+    },
+    [onVerify]
+  )
   return (
     <div className={classNames.container} {...rest}>
-      <ReCAPTCHAComponent
-        size={'normal'}
-        theme="dark"
-        ref={reCaptchaRef}
-        sitekey={siteKey}
-        // onChange={handleChange}
-      />
+      <ReCAPTCHAComponent size={'normal'} theme="dark" sitekey={siteKey} onChange={handleChange} />
       {isError && (
         <div className={classNames.errorText}>Please verify that you are not a robot</div>
       )}
