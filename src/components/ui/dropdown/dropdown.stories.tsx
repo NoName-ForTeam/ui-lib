5a3a043a21c94bea0f210549278971597a6ddcc7
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-22 22:54:49 +0400

    PROJ-172 feat: add DropdownMenu

---
+++ src/components/ui/dropdown/dropdown.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './dropdown'
+import { FillBell } from '../../../assets'
+import { Typography } from '../typography'
+
+const meta = {
+  component: DropdownMenu,
+  tags: ['autodocs'],
+  title: 'Components/DropdownMenu',
+} satisfies Meta<typeof DropdownMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DropdownMenuWithScroll: Story = {
+  render: () => {
+    const itemExample = (
+      <>
+        <Typography variant={'textBold14'} as={'span'}>
+          Новое уведомление!
+        </Typography>
+        <Typography variant={'textSmall'} as={'span'} style={{ color: 'var(--accent-500)' }}>
+          {' Новое'}
+        </Typography>
+        <Typography variant={'textSmall'}>Следующий платеж у вас спишется через 1 день</Typography>
+        <Typography variant={'textSmall'} style={{ color: 'var(--light-900)' }}>
+          1 час назад
+        </Typography>
+      </>
+    )
+
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <FillBell color={'var(--accent-500)'} width={'24px'} height={'24px'} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent label={'Уведомления'}>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+          <DropdownMenuItem>{itemExample}</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  },
+}
---
+++ src/components/ui/dropdown/dropdown.tsx
@@ -0,0 +1,189 @@
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
+import styles from './dropdown.module.scss'
+import { clsx } from 'clsx'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+import s from '@/components/ui/dropdown/dropdown.module.scss'
+import { Scroll } from '@/components'
+
+/**
+ * The root component for the dropdown menu.
+ *
+ * @example
+ * <DropdownMenu>
+ *   <DropdownMenuTrigger> Trigger </DropdownMenuTrigger>
+ *   <DropdownMenuContent label={'Label'}>
+ *     <DropdownMenuItem> Item1 </DropdownMenuItem>
+ *     <DropdownMenuItem> Item2 </DropdownMenuItem>
+ *     <DropdownMenuItem> Item3 </DropdownMenuItem>
+ *   </DropdownMenuContent>
+ * </DropdownMenu>
+ */
+
+export const DropdownMenu = DropdownMenuPrimitive.Root
+
+/**
+ * Trigger component for the dropdown menu.
+ *
+ * @example
+ * <DropdownMenuTrigger className="custom-class"> Trigger </DropdownMenuTrigger>
+ */
+export const DropdownMenuTrigger = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Trigger>
+>(({ className, children, ...restProps }, forwardedRef) => {
+  const classNames = {
+    dropdownMenuTrigger: clsx(styles.dropdownMenuTrigger, className),
+  } as const
+
+  return (
+    <DropdownMenuPrimitive.Trigger
+      asChild
+      ref={forwardedRef}
+      className={classNames.dropdownMenuTrigger}
+      {...restProps}
+    >
+      <button aria-label="Customise options">{children}</button>
+    </DropdownMenuPrimitive.Trigger>
+  )
+})
+DropdownMenuTrigger.displayName = DropdownMenuPrimitive.Trigger.displayName
+
+/**
+ * Type definition for the `DropdownMenuContent` component
+ *
+ * @property {string} [label] - An optional label for the dropdown content.
+ * @extends {ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>}
+ */
+type DropdownMenuContent = {
+  label?: string
+} & ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+
+/**
+ * Content component for the dropdown menu.
+ *
+ * ! Attention: label should be set as props of DropdownMenuItem
+ *
+ * @example
+ * <DropdownMenuContent>
+ *   <DropdownMenuItem label={'Label'}> Item1 </DropdownMenuItem>
+ *   <DropdownMenuItem> Item2 </DropdownMenuItem>
+ *   <DropdownMenuItem> Item3 </DropdownMenuItem>
+ * </DropdownMenuContent>
+ */
+export const DropdownMenuContent = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Content>,
+  DropdownMenuContent
+>(({ label, className, sideOffset = 3, children, ...restProps }, forwardedRef) => {
+  const classNames = {
+    dropdownMenuContent: clsx(styles.dropdownMenuContent, className),
+    dropdownMenuArrowBack: clsx(styles.dropdownMenuArrow, styles.backArrow),
+    dropdownMenuArrowFront: clsx(styles.dropdownMenuArrow, styles.frontArrow),
+    dropdownMenuLabel: clsx(styles.dropdownMenuLabel, className),
+    labelSeparator: styles.labelSeparator,
+  } as const
+
+  return (
+    <DropdownMenuPrimitive.Portal>
+      <DropdownMenuPrimitive.Content
+        ref={forwardedRef}
+        sideOffset={sideOffset}
+        className={classNames.dropdownMenuContent}
+        {...restProps}
+      >
+        <DropdownMenuPrimitive.Arrow className={classNames.dropdownMenuArrowBack} />
+        <DropdownMenuPrimitive.Arrow className={classNames.dropdownMenuArrowFront} />
+        <DropdownMenuPrimitive.Label
+          ref={forwardedRef}
+          className={classNames.dropdownMenuLabel}
+          {...restProps}
+        >
+          {label}
+        </DropdownMenuPrimitive.Label>
+        <DropdownMenuSeparator className={classNames.labelSeparator} />
+        <Scroll className={s.scroll}>{children}</Scroll>
+      </DropdownMenuPrimitive.Content>
+    </DropdownMenuPrimitive.Portal>
+  )
+})
+DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
+
+/**
+ * Label component for the dropdown menu.
+ *
+ * @example
+ * <DropdownMenuLabel inset> Label </DropdownMenuLabel>
+ */
+export const DropdownMenuLabel = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Label>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
+    inset?: boolean
+  }
+>(({ className, ...restProps }, forwardedRef) => {
+  const classNames = {
+    dropdownMenuLabel: clsx(styles.dropdownMenuLabel, className),
+    labelSeparator: styles.labelSeparator,
+  } as const
+
+  return (
+    <>
+      <DropdownMenuPrimitive.Label
+        ref={forwardedRef}
+        className={classNames.dropdownMenuLabel}
+        {...restProps}
+      />
+      <DropdownMenuSeparator className={classNames.labelSeparator} />
+    </>
+  )
+})
+DropdownMenuLabel.displayName = DropdownMenuPrimitive.Label.displayName
+
+/**
+ * Item component for the dropdown menu.
+ *
+ * @example
+ * <DropdownMenuItem> Item </DropdownMenuItem>
+ */
+export const DropdownMenuItem = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item>
+>(({ className, ...restProps }, ref) => {
+  const classNames = {
+    dropdownMenuItem: clsx(styles.dropdownMenuItem, className),
+  } as const
+
+  return (
+    <>
+      <DropdownMenuPrimitive.Item
+        ref={ref}
+        className={classNames.dropdownMenuItem}
+        {...restProps}
+      />
+      <DropdownMenuSeparator />
+    </>
+  )
+})
+DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
+
+/**
+ * Separator component for the dropdown menu.
+ *
+ * @example
+ * <DropdownMenuSeparator />
+ */
+export const DropdownMenuSeparator = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Separator>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
+>(({ className, ...props }, ref) => {
+  const classNames = {
+    dropdownMenuSeparator: clsx(styles.dropdownMenuSeparator, className),
+  } as const
+
+  return (
+    <DropdownMenuPrimitive.Separator
+      ref={ref}
+      className={classNames.dropdownMenuSeparator}
+      {...props}
+    />
+  )
+})
+DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName
