f3b0b1ff07382e365b6c9eb3ac907096d9164391
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-25 17:38:44 +0400

    add: JSDocs

--- src/components/ui/menuMobile/menuMobile.tsx
+++ src/components/ui/menuMobile/menuMobile.tsx
@@ -2,9 +2,20 @@ import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react'
 import styles from './menuMobile.module.scss'
 import clsx from 'clsx'
 
-type MenuMobileProps = ComponentPropsWithRef<'div'>
-
-export const MenuMobile = forwardRef<ElementRef<'div'>, MenuMobileProps>(
+/**
+ * MenuMobile component
+ * is a reusable container component for mobile navigation menus.
+ *
+ * @example
+ * <MenuMobile>
+ *   <HomeOutline width={24} height={24} />
+ *   <PlusSquareOutline width={24} height={24} />
+ *   <MessageCircleOutline width={24} height={24} />
+ *   <Search width={24} height={24} />
+ *   <PersonOutline width={24} height={24} />
+ * </MenuMobile>
+ */
+export const MenuMobile = forwardRef<ElementRef<'div'>, ComponentPropsWithRef<'div'>>(
   ({ children, className }, ref, ...rest) => {
     const classNames = {
       menuMobileContainer: clsx(styles.menuMobileContainer, className),
@@ -17,3 +28,4 @@ export const MenuMobile = forwardRef<ElementRef<'div'>, MenuMobileProps>(
     )
   }
 )
+MenuMobile.displayName = 'MenuMobile'
